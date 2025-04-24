ae6794016172e42cc9d8f2191d4a88020ed3bb07
Mybread2 <ckwnsgh4@naver.com>
2025-04-24 10:09:19 +0900

    클래스생성

---
+++ src/Kiosk/MenuItem.java
@@ -0,0 +1,12 @@
+package Kiosk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuItem {
+
+    public static void main(String[] args) {
+
+        List<MenuItem> menuItems = new ArrayList<>();
+    }
+}
