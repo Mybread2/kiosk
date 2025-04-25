5679bb3d7591d785144ff4e6858666adb991186e
Mybread2 <ckwnsgh4@naver.com>
2025-04-25 15:13:03 +0900

    도전단계 LV.1완료

---
+++ src/Kiosk/DiscountOperation.java
@@ -0,0 +1,37 @@
+package Kiosk;
+
+public class DiscountOperation {
+
+    public enum DiscountType {
+        NationalMerit(10),
+        Soldier(5),
+        Student(3),
+        General(0);
+
+        private final int discountRate;
+
+        DiscountType(int discountRate) {
+            this.discountRate = discountRate;
+        }
+
+        public int getDiscountRate() {
+            return discountRate;
+        }
+
+        public static int getDiscountType(int discountRate) {
+
+            switch (discountRate) {
+                case 1:
+                    return NationalMerit.getDiscountRate();
+                case 2:
+                    return Soldier.getDiscountRate();
+                case 3:
+                    return Student.getDiscountRate();
+                case 4:
+                    return General.getDiscountRate();
+                default:
+                    throw new IllegalArgumentException("올바른 번호를 입력해주세요.");
+            }
+        }
+    }
+}
